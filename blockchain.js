0ea5104f44976db9159d5daf5eddceb5596081e5
emelie.ribring <emelieribring@hotmail.com>
2023-08-28 21:28:14 +0200

    validering

--- blockchain.js
+++ blockchain.js
@@ -1,60 +1,126 @@
 const SHA256 = require('crypto-js/sha256');
+const validator = require('validator');
 
-function Blockchain(){
+function Blockchain() {
     this.chain = [];
-    this.pendeingList = [];
+    this.pendingList = [];
     this.createGenesisBlock();
+    this.difficulty = 4; 
 }
 
 Blockchain.prototype.createGenesisBlock = function () {
     const genesisBlock = {
         index: 0,
-        timestamp: Date.now(),
+        timestamp: Math.floor(Date.now() / 1000),
         data: [],
         nonce: 0,
         previousHash: '0',
-    }
+    };
 
     genesisBlock.hash = this.calculateHash(genesisBlock);
     this.chain.push(genesisBlock);
 };
 
-Blockchain.prototype.createBlock = function (nonce) {
+Blockchain.prototype.createBlock = function () {
     const previousBlock = this.getLastBlock();
-    const block = {
-        index: this.chain.length,
-        timestamp: Date.now(),
-        data: this.pendingList,
-        nonce: nonce,
-        previousHash: previousBlock.hash, 
-    };
+    let nonce = 0;
+    let newBlock = null;
 
-    block.hash = this.calculateHash(block);
+    while (newBlock === null || newBlock.hash.substring(0, this.difficulty) !== Array(this.difficulty + 1).join('0')) {
+        nonce++;
+        newBlock = {
+            index: this.chain.length,
+            timestamp: Math.floor(Date.now() / 1000),
+            data: this.pendingList,
+            nonce: nonce,
+            previousHash: previousBlock.hash,
+        };
+
+        newBlock.hash = this.calculateHash(newBlock);
+    }
 
     this.pendingList = [];
-    this.chain.push(block);
+    this.chain.push(newBlock);
 
-    return block;
+    return newBlock;
 };
 
 Blockchain.prototype.getLastBlock = function () {
     return this.chain[this.chain.length - 1];
 };
 
-Blockchain.prototype.addJournalData = function(patient, message, sender, recipient) {
- const journalData = {
-    patient,
-    message,
-    sender,
-    recipient
- }
 
- this.pendeingList.push(journalData);
+Blockchain.prototype.addJournalData = function(patient, patientDateOfBirth, diagnosis, medicine, message, sender, recipient) {
+    if (!validator.isString(patient) || validator.isEmpty(patient) ||
+        !validator.isString(patientDateOfBirth) || validator.isEmpty(patientDateOfBirth) ||
+        !validator.isString(diagnosis) || validator.isEmpty(diagnosis) ||
+        !validator.isString(medicine) || validator.isEmpty(medicine) ||
+        !validator.isString(message) || validator.isEmpty(message) ||
+        !validator.isString(sender) || validator.isEmpty(sender) ||
+        !validator.isString(recipient) || validator.isEmpty(recipient)) {
+        throw new Error('Ogiltiga eller tomma fält i journaldata.');
+    }
 
- return this.getLastBlock()['index'] + 1;
+    const sanitizedPatient = validator.escape(patient);
+    const sanitizedPatientDateOfBirth = validator.escape(patientDateOfBirth);
+    const sanitizedDiagnosis = validator.escape(diagnosis);
+    const sanitizedMedicine = validator.escape(medicine);
+    const sanitizedMessage = validator.escape(message);
+    const sanitizedSender = validator.escape(sender);
+    const sanitizedRecipient = validator.escape(recipient);
+
+    const journalData = {
+        patient: sanitizedPatient,
+        patientDateOfBirth: sanitizedPatientDateOfBirth,
+        diagnosis: sanitizedDiagnosis,
+        medicine: sanitizedMedicine,
+        message: sanitizedMessage,
+        sender: sanitizedSender,
+        recipient: sanitizedRecipient
+    };
+
+    this.pendingList.push(journalData);
+
+    return this.getLastBlock()['index'] + 1;
 };
 
+
 Blockchain.prototype.calculateHash = function (block) {
+    let calculatedHash = "";
+    let prefix = "0".repeat(this.difficulty);
+
+    while (calculatedHash.substring(0, this.difficulty) !== prefix) {
+        block.nonce++;
+        calculatedHash = SHA256(
+            block.index +
+            block.previousHash +
+            block.timestamp +
+            JSON.stringify(block.data) +
+            block.nonce
+        ).toString();
+    }
+
+    return calculatedHash;
+};
+
+Blockchain.prototype.validateChain = function () {
+    for (let i = 1; i < this.chain.length; i++) {
+        const currentBlock = this.chain[i];
+        const previousBlock = this.chain[i - 1];
+
+        if (currentBlock.hash !== this.calculateHashWithoutPoW(currentBlock)) {
+            return false;
+        }
+
+        if (currentBlock.previousHash !== previousBlock.hash) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+Blockchain.prototype.calculateHashWithoutPoW = function (block) {
     return SHA256(
         block.index +
         block.previousHash +
--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); 
+
 const Blockchain = require('./blockchain');
 
 const journalChain = new Blockchain();
@@ -6,11 +11,29 @@ journalChain.createBlock(1, 'previousHash', 'currentHash');
 journalChain.addJournalData('Matilda', 'Blablabla', 'Emelie', 'Danderyd Hospital');
 journalChain.createBlock(2, 'previousHash', 'currentHash');
 journalChain.createBlock(3, 'previousHash', 'currentHash');
-journalChain.createBlock(3, 'previousHash', 'currentHash');
+journalChain.createBlock(4, 'previousHash', 'currentHash');
+journalChain.addJournalData('Isabell', 'litet meddelande', 'Ida', 'Södersjukhuset');
+journalChain.createBlock(5, 'previousHash', 'currentHash');
+
+app.post('/addData', (req, res) => {
+  const { patient, message, sender, recipient } = req.body;
+  const newIndex = journalChain.addJournalData(patient, message, sender, recipient); 
+  res.json({ message: `Journaldata kommer att läggas till i block ${newIndex}` });
+});
+
+app.get('/getBlockchain', (req, res) => {
+  res.json(journalChain.chain); 
+});
+
+app.get('/validateBlockchain', (req, res) => {
+  const isValid = journalChain.validateChain(); 
+  res.json({ isValid: isValid });
+});
+
+app.listen(3000, () => console.log('Server kör på port 3000'));
 
 
 
-
-
+/*  
 console.log(journalChain);
-console.log(journalChain.chain.at(1));
+console.log(journalChain.chain.at(2)); */
